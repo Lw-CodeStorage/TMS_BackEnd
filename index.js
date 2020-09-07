4a9b36c7af0d032aa3f7d76141afd27d4dbed984
Leo <st831209@gmail.com>
2020-09-07 18:22:25 +0800

    修改了mysql物件結構 變成用export輸出

--- index.js
+++ index.js
@@ -3,32 +3,36 @@ let bodyParser = require('body-parser');
 let cors = require('cors');
 let mysql = require('./mysql');
 const { resolve } = require('path');
-let DB = new mysql()
 
-console.log('NodeJS Start');
 let app = express();
 app.use(cors())
 app.use(bodyParser.json({ extended: false }));
-app.post('/', async function (req, res) {
-       
-        try {
-            let result = await DB.init().register(req.body.name, req.body.password, req.body.email);
-            console.log(result);
-            res.send( JSON.stringify({"狀態":result}))
-        } catch (error) {
-            console.log(error);
-            res.send( JSON.stringify({"狀態":error}))
+app.post('/register', async function (req, res) {
+
+    try {
+        let result = await mysql.register(req.body.name, req.body.password, req.body.email, req.body.phone);
+        console.log(result);
+        if (result == '信箱重複') {
+            res.send(JSON.stringify({ "狀態": '註冊異常', '訊息': result }))
+        } else {
+            res.send(JSON.stringify({ "狀態": '註冊成功', '訊息': result }))
         }
+    } catch (error) {
+        console.log(error);
+        res.send(JSON.stringify({ "狀態": error }))
+    }
 });
-app.post('/login' ,async (req,res)=>{
-        try {
-            let result = await DB.init().login(req.body.email, req.body.password);
-           console.log(result);
-            res.send( JSON.stringify({"狀態":result}))
-        }catch(error){
-            console.log(error);
-            res.send( JSON.stringify({"狀態":error}))
-        }
+app.post('/login', async (req, res) => {
+    try {
+        let result = await mysql.login(req.body.email, req.body.password);
+        console.log(result);
+        result == '帳號或密碼不存在'? 
+        res.send(JSON.stringify({ "狀態": '登入異常','訊息': result })):
+        res.send(JSON.stringify({ "狀態": '註冊成功', '訊息': '登入成功' }))
+    } catch (error) {
+        console.log(error);
+        res.send(JSON.stringify({ "狀態": error }))
+    }
 })
 
 
@@ -36,7 +40,5 @@ app.listen(8888, function () {
     console.log('CORS-enabled web server listening on port 8888')
 });
 
-// 终端打印如下信息
-//console.log('Server running at http://localhost:8888/');
 
 
--- mysql.js
+++ mysql.js
@@ -1,63 +1,58 @@
 var mysql = require('mysql');
-const { threadId } = require('worker_threads');
-function DB() {
-    //屬性
-    this.connection
-    //方法
-    this.init = function () {
-        this.connection = mysql.createConnection({
-            host: '60.249.180.235',
-            port: '3306',
-            user: 'fois_ios',
-            password: 'fois_ios',
-            database: 'fois_ios',
-        });
-        this.connection.connect(function (err) {
-            if (err) throw err;
-            console.log('connect success!');
-        });
-        return this
-    }
 
-    this.register = (userName, password, email, Authority) => {
-        return new Promise((resolve, reject) => {
-                this.connection.query(`SELECT email FROM users where email = '${email}'`, (error, result, fields) => {
-                    if (result.length) {
-                        resolve('信箱重複')
-                    } else {
-                        this.connection.query(`INSERT INTO users ( userName, passWord, email, Authority) VALUES ('${userName}', '${password}', '${email}', '學生')`,
-                            (error, results, fields) => {
+let connection = mysql.createConnection({
+    host: '60.249.180.235',
+    port: '3306',
+    user: 'fois_ios',
+    password: 'fois_ios',
+    database: 'fois_ios',
+});
 
-                                if (error) {
-                                    //console.log(error);
-                                    reject('註冊失敗')
-                                }
-                                //console.log('insert');
-                                resolve('註冊成功')
+let register = (userName, password, email, Authority) => {
 
-                            })
-                        this.connection.end();
-                    }
-                })
-            })
-    }
-    this.login = (email ,password) =>{
-        return new Promise((resolve,reject)=>{
-            this.connection.query(`SELECT  Authority FROM users  WHERE email='${email}' AND passWord='${password}'`,(error,result,fields)=>{
-                if(result.length == 0){
-                    //console.log(result);
-                    resolve('帳號或密碼不存在')
-                }else{
-                    if(error){
-                        reject('登入發生錯誤')
-                    }
-                    
-                    resolve(result[0]['Authority'])
-                }
-            })
-            this.connection.end();
+    return new Promise((resolve, reject) => {
+        connection.query(`SELECT email FROM users where email = '${email}'`, (error, result, fields) => {
+            if (result.length) {
+                resolve('信箱重複')
+            } else {
+                connection.query(`INSERT INTO users ( userName, passWord, email, Authority) VALUES ('${userName}', '${password}', '${email}', '學生')`,
+                    (error, results, fields) => {
+
+                        if (error) {
+                            //console.log(error);
+                            reject('註冊失敗')
+                        }
+                        //console.log('insert');
+                        resolve('註冊成功')
+
+                    })
+            }
+
         })
-    }
+
+    })
+    connection.end();
 }
 
-module.exports = DB
+let login = (email, password) => {
+    return new Promise((resolve, reject) => {
+        connection.query(`SELECT  Authority FROM users  WHERE email='${email}' AND passWord='${password}'`, (error, result, fields) => {
+            if (result.length == 0) {
+                //console.log(result);
+                resolve('帳號或密碼不存在')
+            } else {
+                if (error) {
+                    reject('登入發生錯誤')
+                }
+
+                resolve(result[0]['Authority'])
+            }
+        })
+
+    })
+    connection.end();
+}
+
+
+
+module.exports = { register: register, login: login }
